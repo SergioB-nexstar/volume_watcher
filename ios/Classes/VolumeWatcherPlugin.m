204f493e82a9a750a63d88db00e063fa8c933946
Sergio Bernal <c-sbernal@nexstar.tv>
2021-10-11 15:20:57 -0500

    Added NSKeyValueObservingOptionInitial option

--- ios/Classes/VolumeWatcherPlugin.m
+++ ios/Classes/VolumeWatcherPlugin.m
@@ -122,12 +122,12 @@
     // 添加监听系统音量变化
     
         
-//    [[NSNotificationCenter defaultCenter] addObserver:self
-//                                             selector:@selector(onVolumeChanged:)
-//                                                 name:@"AVSystemController_SystemVolumeDidChangeNotification"
-//                                               object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(onVolumeChanged:)
+                                                 name:@"AVSystemController_SystemVolumeDidChangeNotification"
+                                               object:nil];
     
-    [[AVAudioSession sharedInstance] addObserver:self forKeyPath:@"outputVolume" options: NSKeyValueObservingOptionNew context:nil];
+    [[AVAudioSession sharedInstance] addObserver:self forKeyPath:@"outputVolume" options: (NSKeyValueObservingOptionNew | NSKeyValueObservingOptionInitial) context:nil];
     
     // 需要开启该功能以便监听系统音量
     [[UIApplication sharedApplication] beginReceivingRemoteControlEvents];
@@ -135,6 +135,7 @@
 }
 
 - (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context {
+    NSLog(@"SERGIO: observeValueForKeyPath");
     if ([keyPath isEqualToString:@"outputVolume"]){
         float currentVol = [[AVAudioSession sharedInstance] outputVolume];
         _eventSink(@(currentVol));
